7f2227b325237144eab4b6dd077d7eef5d2ca3de
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-09-11 22:26:57 +0530

    setup react router with nested layouts, pages and context API state management

--- web-app-week-03/src/App.tsx
+++ web-app-week-03/src/App.tsx
@@ -1,13 +1,46 @@
-import Card from "./components/Card"
+// import CompA from "./components/CompA"
 
-const App = () => {
-  const name = "Chathunga"
+// const App = () => {
+//   return (
+//     <>
+//       <div className="bg-yellow-300 h-screen p-2">
+//         <h1 className="text-white">App</h1>
+//         <CompA />
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom"
+import { lazy, Suspense } from "react"
+
+import DashboarLayout from "./layouts/DashboardLayout"
+import AUthDashboard from "./layouts/AuthDashboard"
+
+const Home = lazy(() => import("./pages/home"))
+const About = lazy(() => import("./pages/about"))
+const Me = lazy(() => import("./pages/me"))
+const Login = lazy(() => import("./pages/login"))
+
+function App() {
   return (
-    <>
-      <div></div>
-      <Card data={name}/>
-      <Card age = {18}></Card>
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<DashboarLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/me" element={<Me />} />
+          </Route>
+          <Route element={<AUthDashboard />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<div>Reg</div>} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
